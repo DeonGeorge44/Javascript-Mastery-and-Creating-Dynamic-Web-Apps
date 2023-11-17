05fc71335e5dca9daff357cfb55cd08ef54af55b
DeonGeorge44 <ddeongeorge44@gmail.com>
2023-11-17 18:26:22 +0530

    functions & Scopes

--- 14_Functions.js
+++ 14_Functions.js
@@ -10,7 +10,48 @@ DRY -- Don't Repeat Yourself
 
 */
 
+//------Function-Declaration--------------------------
 function greet() {
     console.log("Hello From Function");
 }
 greet(); //call,run/execute a funtion(invoke a function)
+
+//-------------Functions with parameters----------------------------------
+
+function sayHello(name) {
+    console.log("hello my name is", name);
+}
+sayHello("Deon_George");
+sayHello("Alex");
+
+//return keyword in functions      --- returns some data from the function
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(3, 10));
+
+const res1 = add(5, 10);
+const res2 = add(88, 100);
+const res3 = add(500, 44);
+console.log(res1);
+console.log(res2);
+console.log(res3);
+
+//-----------------------------Function Expression------------------------
+//Function Expression is a function which we store inside a variable
+
+//Example
+
+const Man = function (name) {
+    return name;
+};
+
+console.log(Man("DEon"));
+
+//Summary ---
+/* how we are declaring the function
+   how to store value inside a function
+   parameters and arguments
+   return keyword
+ */
