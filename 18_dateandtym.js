0cdd22a351e86a45536d5f871a9a6a57e05bd78e
DeonGeorge44 <ddeongeorge44@gmail.com>
2023-11-18 23:49:07 +0530

    error-correction

---
+++ 18_dateandtym.js
@@ -0,0 +1,6 @@
+//--------------------------Date & Time In Javascript----------------------------------------------------------------------------
+//Date Object allow you to create,manipulate and format the date and time in Javascript
+
+const currentDate = new Date();
+console.log(currentDate);
+console.log(currentDate);
