58d36f749ac365a4de4ca0e1cf527a46d1eef691
DeonGeorge44 <ddeongeorge44@gmail.com>
2023-11-21 14:49:18 +0530

    till-enhanced-object-literals-

---
+++ 19.1_challenge14.js
@@ -0,0 +1,23 @@
+// -------------Template Strings Exercise---------------------------------------
+
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+const sentence = `The Quick 
+brown fox 
+jumps 
+over the lazy dog`;
+
+console.log(sentence);
+
+const firstName = `Deon`;
+const lastName = `George`;
+
+const name = `My Name is ${firstName} ${lastName}`;
+console.log(name);
---
+++ 21_enhancedObjectLiterals.js
@@ -0,0 +1,5 @@
+//----------------------------Enhanced Object Literals----------------------------------------------------------------------------
+/*
+Enhanced Object Literal,introduced in ECMASCRIPT 6 (ES6), are a set of enhancements to the syntax for defining objects in Javascript.
+These enhancements make it more convenient and concise  to define object properties and methods.
+*/
